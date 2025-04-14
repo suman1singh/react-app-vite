266a56444eb69e5e5604f0b6e14176fd6bee167e
Suman Singh <sunnysuman132@gmail.com>
2025-04-15 02:15:53 +0530

    vite docs

--- react-app-vite/src/App.tsx
+++ react-app-vite/src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//In React.js development, Vite is a modern frontend build tool and
+//development server that provides a fast and optimized experience,
+//especially during development.
+
+// Why Vite is used in React?
+// Here’s what makes Vite great for React projects:
+
+// 🔥 1. Lightning Fast Development Server
+// Uses native ES Modules (ESM) in the browser.
+// No bundling needed during development – changes reflect almost instantly.
+
+// ⚡️ 2. Fast Hot Module Replacement (HMR)
+// React component changes update in the browser without full reload.
+// Much faster than Webpack-based setups.
+
+// 📦 3. Optimized Production Build
+// Uses Rollup under the hood for bundling.
+// Produces smaller and more efficient production code.
+
+// 🧠 4. Simplified Configuration
+// Less boilerplate than Webpack or Create React App (CRA).
+// Supports JSX, TypeScript, CSS modules, and more out of the box.
+
+//Summary:
+// Dev speed: Vite is super fast, while Webpack/CRA can be slower in large projects.
+// HMR (Hot Module Replacement.): Vite offers fast and reliable hot module replacement; Webpack/CRA can be slower.
+// Config: Vite requires minimal configuration, whereas Webpack/CRA is often complex.
+// Build Tool: Vite uses Rollup for production builds, while Webpack/CRA uses Webpack.
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>What is vite in Reactjs</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
